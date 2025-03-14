a3177e7c8bc3355bcf1fa8fd8d47d48a83a506be
sohaibjoma <sohaibjoma@gmail.com>
2025-03-14 02:20:26 +0200

    Worked on employment types

--- src/router/modules/layouts/mainLayout.js
+++ src/router/modules/layouts/mainLayout.js
@@ -1,23 +1,26 @@
 //? the dahshboard layout including the child components that would appear inside this layout
 
-import mainLayout from "../../../layouts/mainLayout/index.vue"
-import profileRoute from "../profile"
-import settingsRoutes from "../settings"
-import TitlesRoutes from "../Titles"
-import CountriesRoutes from "../countries"
-import visaTypesRoutes from "../visa-types"
-import UsersRoutes from "../users"
+import mainLayout from "../../../layouts/mainLayout/index.vue";
+import profileRoute from "../profile";
+import settingsRoutes from "../settings";
+import TitlesRoutes from "../Titles";
+import CountriesRoutes from "../countries";
+import visaTypesRoutes from "../visa-types";
+import UsersRoutes from "../users";
+import EmploymentTypesRoutes from "../employment-types";
 
 const mainLayoutRoute = {
-  path: '/dashboard', component: mainLayout,
-  children:[
+  path: "/dashboard",
+  component: mainLayout,
+  children: [
     profileRoute,
     ...settingsRoutes,
     ...TitlesRoutes,
     ...CountriesRoutes,
     ...visaTypesRoutes,
-    ...UsersRoutes
-  ]
-}
+    ...UsersRoutes,
+    ...EmploymentTypesRoutes,
+  ],
+};
 
-export default mainLayoutRoute
+export default mainLayoutRoute;
